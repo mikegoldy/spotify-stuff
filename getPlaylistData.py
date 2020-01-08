c6a14f4b419e9a49b9c3f4ad3b7ee6f1dde021c6
Michael_Goldfeld <michaelgoldfeld@gmail.com>
2020-01-08 18:17:42 -0500

    Add comments and explanation to getPlaylistData.py & createSpotifyLibrary.py

--- getPlaylistData.py
+++ getPlaylistData.py
@@ -1,3 +1,24 @@
+"""
+Takes a library and compiles a csv with useful data:
+name
+artist
+duration
+key
+mode
+beats
+acousticness
+danceability
+energy
+instrumentalness
+liveness
+loudness
+speechiness
+valence
+tempo
+popularity
+
+Precondtion: supply program with playlist id by moddifying variable: playlist_id
+"""
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
@@ -12,6 +33,9 @@ user = sp.current_user()["id"]
 numOfSongs = 0
 
 class variables:
+    """
+    Stores list of each song and corresponding data
+    """
     uri = []
     name = []
     artist = []
@@ -31,6 +55,10 @@ class variables:
     popularity = []
 
 def main():
+    """
+    Loops through entire playlist and calls function fillVariables for each song.
+    After it finishes looping, it breaks loop and calls fillCsv to create csv for all the variables
+    """
     global numOfSongs
     i=0
     while True:
@@ -50,6 +78,9 @@ def main():
 Vars = variables()
 
 def fillVariables(song, result, features, i):
+    """
+    Appends each data element to object Vars of class variable
+    """
     Vars.uri.append(song)
     Vars.name.append(result["items"][i]["track"]["name"])
     Vars.artist.append(result["items"][i]["track"]["artists"][0]["name"])
@@ -69,6 +100,7 @@ def fillVariables(song, result, features, i):
     Vars.popularity.append(result["items"][i]["track"]["popularity"])
 
 def fillCsv():
+    "Fills out csv of all data"
     data = pd.DataFrame(list(zip(Vars.name, Vars.artist, Vars.duration, Vars.key, Vars.mode,
     Vars.beats, Vars.acousticness, Vars.danceability, Vars.energy, Vars.instrumentalness,
     Vars.liveness, Vars.loudness, Vars.speechiness, Vars.valence, Vars.tempo, Vars.popularity, Vars.uri)),
